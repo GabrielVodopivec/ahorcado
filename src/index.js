a73206cf37bcc4fb5d67d23f45970b69dab1a0b7
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-08-08 06:38:16 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,173 @@
+const listaDePalabras = [
+    "CAMAROGRAFO",
+    "GALAXIA",
+    "MINOTAURO",
+    "HOMOSAPIENS",
+    "PROGRAMADOR",
+    "ENTRETENIMIENTO",
+    "REACCIONAR",
+    "AEROPLANO",
+    "DESAYUNAR",
+    "EMBARCACION",
+    "Electrocardiograma",
+    "monumental",
+    "terrestres",
+    "paciencia",
+    "fundamento",
+    "reemplazo",
+    "coronado",
+    "pantalones",
+    "Termoestable",
+    "ordenadores"
+];
+
+const randomWord = (max) => {
+    let randomNumber = Math.floor(Math.random()*max); 
+    let randomWord = listaDePalabras[randomNumber].toUpperCase();
+    return randomWord;
+} 
+    
+let word = randomWord(listaDePalabras.length);
+
+let oportunidades = 1;
+let aciertos = [];
+
+let letterContainer = document.getElementById("letterContainer");
+let reset = document.getElementById("reset");
+let hidennWord = document.getElementById("hidennWord");
+let img = document.getElementById("img");
+img.src = "./assests/01.PNG";
+
+
+for (let i = 0; i < word.length; i++) {
+    let space = document.createElement("div");
+    space.innerHTML = "";
+    space.className = "hiddenLetter";
+    space.id = i;
+    hidennWord.appendChild(space);
+}
+
+for (let i = 65; i < (65 + 26); i++) {
+    let newButton = document.createElement("button");
+    let letter = String.fromCharCode(i);
+
+    newButton.innerText = letter;
+    newButton.id = letter;
+    newButton.className = "button";
+    
+    letterContainer.appendChild(newButton);
+
+}
+
+function listener(event) {
+
+    let letter = event.target.innerHTML;
+    let flag = true;
+
+    for (let i = 0; i < word.length; i++) {
+
+        if (word[i] === letter) {
+
+            let space = document.getElementById(i);
+
+            space.innerHTML = letter;
+            space.className = "hiddenLetterGuessed";
+            event.path[0].disabled = true;
+            event.path[0].className = "buttonDisabled"
+            flag = false;
+            aciertos.push(letter)
+        }
+    }
+
+    if (flag) {
+        oportunidades++
+
+        if (oportunidades === 7) {
+
+            let message = document.createElement("h1");
+    
+            message.textContent = "PERDISTE!!!!";
+            message.className = "message";
+            message.id = "message";
+            
+            document.body.appendChild(message);
+            
+            for (let i = 65; i < (65 + 26); i++) {
+                document.getElementById(
+                    String.fromCharCode(i)
+                    ).onclick = null;
+                }
+
+            return img.src = "./assests/gifAhorcado.gif"
+        }
+
+        img.src = `./assests/0${oportunidades}.PNG`
+    }
+    
+
+    if (aciertos.length === word.length) {
+        img.src = `./assests/rumba-fiesta.gif`
+        let message = document.createElement("h1");
+
+        message.textContent = "ADIVINASTE!!!!";
+        message.className = "message";
+        message.id = "message";
+
+        document.body.appendChild(message);
+
+        for (let i = 65; i < (65 + 26); i++) {
+            let button = document.getElementById(String.fromCharCode(i))
+            button.onclick = null;
+        }
+    }    
+    
+
+}
+
+
+
+const resetFunction = () => {
+
+    oportunidades = 1;
+    aciertos = [];
+    img.src = "./assests/01.PNG";
+
+    for (let i = 0; i < word.length; i++) {
+        hidennWord.removeChild(
+            document.getElementById(i)
+        );
+    }
+
+    word = randomWord(listaDePalabras.length);
+
+    for (let i = 0; i < word.length; i++) {
+        let space = document.createElement("div");
+        space.innerHTML = "";
+        space.className = "hiddenLetter";
+        space.id = i;
+        hidennWord.appendChild(space);
+    }
+
+    for (let i = 65; i < (65 + 26); i++) {
+        let button = document.getElementById(String.fromCharCode(i))
+        button.disabled = false;
+        button.onclick = listener;
+    }
+
+    let message = document.getElementById("message")
+    
+    if(message) {
+        document.body.removeChild(message)
+    }
+    
+}
+
+for (let i = 65; i < (65 + 26); i++) { 
+    document.getElementById(
+        String.fromCharCode(i)
+        ).onclick = listener;
+}
+
+reset.onclick = resetFunction;
+console.log(word)
+
---
+++ src/index3.js
@@ -0,0 +1,115 @@
+
+
+const word = "ESTEREOTIPO";
+
+let oportunidades = 1;
+let aciertos = [];
+
+let letterContainer = document.getElementById("letterContainer");
+let reset = document.getElementById("reset");
+let hidennWord = document.getElementById("hidennWord");
+let img = document.getElementById("img");
+img.src = "./assests/01.PNG";
+
+
+for (let i = 0; i < word.length; i++) {
+    let space = document.createElement("div");
+    space.innerHTML = "";
+    space.className = "hiddenLetter";
+    space.id = i;
+    hidennWord.appendChild(space);
+}
+
+for (let i = 65; i < (65 + 26); i++) {
+    let newButton = document.createElement("button");
+    let letter = String.fromCharCode(i);
+
+    newButton.innerText = letter;
+    newButton.id = letter;
+    newButton.className = "button";
+    
+    letterContainer.appendChild(newButton);
+
+}
+function listener(event) {
+
+    if (
+        (event.path[0].id != "reset") && 
+        (event.path[0].id != "img") && 
+        isNaN(event.path[0].id) && 
+        (oportunidades < 7) && 
+        (aciertos.length != word.length)
+        ) {
+
+            let letter = event.target.innerHTML;
+            let flag = true;
+
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] === letter) {
+                    let space = document.getElementById(i);
+                    space.innerHTML = letter;
+                    event.path[0].disabled = true
+                    flag = false;
+                    aciertos.push(letter)
+                }
+            }
+
+        if (flag) {
+            oportunidades++
+            img.src = `./assests/0${oportunidades}.PNG`
+        }
+    }
+
+    if (aciertos.length === word.length) {
+        let message = document.createElement("h1");
+        message.textContent = "ADIVINASTE!!!!";
+        message.className = "message";
+        message.id = "message";
+        document.body.appendChild(message);
+        document.removeEventListener("click", listener)
+    }    
+    
+    if (oportunidades === 7) {
+        let message = document.createElement("h1");
+        message.textContent = "PERDISTE!!!!";
+        message.className = "message";
+        message.id = "message";
+        document.body.appendChild(message)
+        document.removeEventListener("click", listener)
+    }
+    console.log(oportunidades)
+    console.log(isNaN(event.path[0].id))
+    console.log(event.path[0].id)
+}
+
+
+const resetFunction = () => {
+
+    oportunidades = 1;
+    img.src = "./assests/01.PNG";
+    aciertos = [];
+    
+    for (let i = 0; i < word.length; i++) {
+        document.getElementById(i).innerHTML = "";
+    }
+
+    for (let i = 65; i < (65 + 26); i++) {
+        document.getElementById(
+            String.fromCharCode(i)
+            ).disabled = false;
+    }
+
+    let message = document.getElementById("message")
+    
+    if(message) {
+        document.body.removeChild(message)
+    }
+    
+    document.addEventListener('click', listener);
+    
+}
+
+document.addEventListener('click', listener )
+reset.onclick = resetFunction;
+
+
